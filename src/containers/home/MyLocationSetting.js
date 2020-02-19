9cdf064547896cd633dbc9177243272d8374f841
juntaekHong <wnsxor3489@gmail.com>
2020-02-19 18:19:25 +0900

    내 위치 설정을 위한 모듈 추가

--- src/containers/home/MyLocationSetting.js
+++ src/containers/home/MyLocationSetting.js
@@ -1,15 +1,40 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useRef, useState} from 'react';
+import Styled from 'styled-components/native';
 import {View, TouchableOpacity, Text} from 'react-native';
 import {TopContainerView, TopView} from '../../components/common/View';
 import {widthPercentageToDP} from '../../utils/util';
+// 내 위치 정보 확인
+import Geolocation from '@react-native-community/geolocation';
+
+const Label = Styled.Text`
+    font-size: 24px;
+`;
 
 const MyLocationSetting = props => {
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+        });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
+
   return (
     <TopContainerView>
       <TopView
         marginBottom={5}
-        title={'위치 설정 페이지'}
+        title={'My Location Setting Page'}
         backHandler={() => {
           props.navigation.goBack(null);
         }}
@@ -17,7 +42,23 @@ const MyLocationSetting = props => {
           props.navigation.goBack(null);
         }}
       />
+      {/* 지도 범위 뷰 작업중 */}
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: '100%',
+          height: widthPercentageToDP(250),
+        }}
+      />
       <Text>위치 설정 페이지</Text>
+      {location ? (
+        <>
+          <Label>Latitude: {location.latitude}</Label>
+          <Label>Latitude: {location.longitude}</Label>
+        </>
+      ) : (
+        <Label>Loading...</Label>
+      )}
     </TopContainerView>
   );
 };
