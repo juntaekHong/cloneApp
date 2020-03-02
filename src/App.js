5fda8a5f4b94ba67f5a909dcfbf3dd5fb87ee3c3
juntaekHong <wnsxor3489@gmail.com>
2020-03-02 14:47:23 +0900

    앱 실행 시, 첫화면 앱 아이콘 띄우기 // 필요 시 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Platform, View, StyleSheet, Text, SafeAreaView} from 'react-native';
+import {
+  Platform,
+  View,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+} from 'react-native';
 import AppNavigation from './navigations';
 import SplashScreen from 'react-native-splash-screen';
 import {useScreens} from 'react-native-screens';
@@ -10,19 +18,49 @@ import {connect} from 'react-redux';
 // import {CenterView} from './components/common/View';
 import {UIActivityIndicator} from 'react-native-indicators';
 import Modal from 'react-native-modal';
+import {widthPercentageToDP} from './utils/util';
 //붙여넣기 기능 쓰려면 이 놈 주석처리
 // useScreens();
 
 class App extends Component {
   constructor(props) {
     super(props);
-    SplashScreen.hide();
+    // SplashScreen.hide();
+
+    this.state = {
+      firstScreen: true,
+    };
+  }
+
+  async componentDidMount() {
+    let timeout = setInterval(() => {
+      this.setState({firstScreen: false});
+      clearInterval(timeout);
+    }, 2000);
   }
 
   render() {
     const {loading} = this.props;
     return (
       <SafeAreaView style={styles.container}>
+        <Modal
+          isVisible={this.state.firstScreen}
+          style={{
+            margin: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'white',
+          }}>
+          <View backgroundColor={'white'}>
+            <Image
+              style={{
+                width: widthPercentageToDP(100),
+                height: widthPercentageToDP(100),
+              }}
+              source={require('../assets/image/hospital.png')}
+            />
+          </View>
+        </Modal>
         <Modal isVisible={loading}>
           <View backgroundColor={'transparent'}>
             <UIActivityIndicator color={'gray'} />
