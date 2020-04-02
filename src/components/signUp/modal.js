224ae4d939f081293e3028917d41266945408764
juntaekHong <wnsxor3489@gmail.com>
2020-04-02 14:49:28 +0900

    유효성 검사 정규식 코드 추가 및 회원가입 유효성 검사 기능 추가  다음 할 일 1. 회원가입, 로그인 기능 구현 2. 코드 정리 3. 예약페이지 작업

---
+++ src/components/signUp/modal.js
@@ -0,0 +1,87 @@
+import React, {useState, useCallback} from 'react';
+import styled from 'styled-components/native';
+import {CustomModal} from '../common/Modal';
+import {NBGText} from '../common/Text';
+import Picker from 'react-native-wheel-picker-extended';
+import {widthPercentageToDP} from '../../utils/util';
+import fonts from '../../configs/fonts';
+import colors from '../../configs/colors';
+import FastImage from 'react-native-fast-image';
+var PickerItem = Picker.Item;
+
+const PickerCheckerView = styled.View`
+  width: 100%
+  height: ${widthPercentageToDP(64)}
+  justify-content: center
+  align-items: center
+  border-bottom-left-radius: ${widthPercentageToDP(14)}
+  border-bottom-right-radius: ${widthPercentageToDP(14)}
+`;
+
+const PickerChecker = styled.TouchableOpacity`
+  width: ${widthPercentageToDP(35)}
+  height: ${widthPercentageToDP(35)}
+`;
+
+const PickerFooter = ({footerHandler}) => {
+  return (
+    <PickerCheckerView>
+      <PickerChecker onPress={footerHandler}>
+        <FastImage
+          style={{
+            width: widthPercentageToDP(35),
+            height: widthPercentageToDP(35),
+          }}
+          source={require('HandamProject/assets/image/sign/selection.png')}
+        />
+      </PickerChecker>
+    </PickerCheckerView>
+  );
+};
+
+export const WheelPicker = props => {
+  const [value, setValue] = useState(
+    props.value === null && props.data.length > 0
+      ? 0
+      : props.data.indexOf(props.value + ''),
+  );
+  const onValueChange = useCallback(index => {
+    setValue(index);
+  }, []);
+
+  const handleChangeValue = async () => {
+    if (value === null) await props.footerHandler(props.value);
+    else await props.footerHandler(props.data[value]);
+    props.closeHandler();
+  };
+
+  handleCloseModal = () => {
+    if (props.value === null) setValue(0);
+    else setValue(props.data.indexOf(props.value));
+    props.closeHandler();
+  };
+
+  return (
+    <CustomModal
+      {...props}
+      closeHandler={handleCloseModal}
+      renderFooter={() =>
+        PickerFooter({...props, footerHandler: handleChangeValue})
+      }>
+      <Picker
+        style={{width: '100%', height: '100%'}}
+        lineColor={'#cecece'}
+        selectedValue={value}
+        itemStyle={{
+          color: colors.black,
+          fontSize: widthPercentageToDP(22),
+          fontFamily: fonts.nanumBarunGothic,
+        }}
+        onValueChange={onValueChange}>
+        {props.data.map((value, i) => (
+          <PickerItem label={value} value={i} key={i + value} />
+        ))}
+      </Picker>
+    </CustomModal>
+  );
+};
--- src/containers/myPage/SignUp.js
+++ src/containers/myPage/SignUp.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {widthPercentageToDP} from '../../utils/util';
 import {
   TopContainerView,
@@ -8,21 +8,131 @@ import {
   BTN,
 } from '../../components/common/View';
 import {NBGBText} from '../../components/common/Text';
-import {TextInput, Keyboard, ScrollView} from 'react-native';
+import {TextInput, Keyboard, ScrollView, View} from 'react-native';
 import {UnSelectImg, SelectImg} from '../../components/home/Image';
 import colors from '../../configs/colors';
 import Swiper from 'react-native-swiper';
+import {
+  checkPass,
+  checkPassCompare,
+  checkName,
+  checkNickNameLength,
+  checkNickName,
+  checkEmail,
+  checkPhoneNumber,
+} from '../../utils/validation';
+// import {WheelPicker} from '../../components/signUp/modal';
 
 const SignUp = props => {
   const [index, setIndex] = useState(0);
 
+  // 아이디, 아이디 유효성
   const [id, setId] = useState('');
+  const [idValid, setIdValid] = useState('');
+  // 비밀번호, 비밀번호 유효성
   const [pass, setPass] = useState('');
+  const [passValid, setPassValid] = useState('');
+  // 비밀번호 재확인, 비밀번호 일치 유효성
   const [passCheck, setPassCheck] = useState('');
+  const [passSame, setPassSame] = useState('');
 
+  // 비밀번호 보이기
   const [passVisible, setPassVisible] = useState(true);
 
+  // 이름, 이름 유효성
+  const [name, setName] = useState('');
+  const [nameValid, setNameValid] = useState('');
+
+  // 닉네임, 닉네임 유효성
+  const [nickName, setNickName] = useState('');
+  const [nickNameValid, setNickNameValid] = useState('');
+
+  // 나이, 나이 유효성
+  const [age, setAge] = useState('');
+  const [ageValid, setAgeValid] = useState('');
+  // const [ageModal, setAgeModal] = useState(false);
+
+  // 성별
+  const [gender, setGender] = useState(null);
+
+  // 전화번호
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [phoneNumberValid, setPhoneNumberValid] = useState('');
+
+  // 이메일
+  const [email, setEmail] = useState('');
+  const [emailValid, setEmailValid] = useState('');
+
+  // 아바타
+
   const swipe = useRef();
+
+  // 아이디 입력시, 아이디 유효성 체크
+  useEffect(() => {
+    if (id.length !== 0 && id.length < 3) {
+      setIdValid('* 3글자 이상 입력해주세요.');
+    } else {
+      setIdValid('');
+    }
+  }, [id]);
+
+  // 비밀번호 입력시, 비밀번호 유효성 체크
+  useEffect(() => {
+    let valid = checkPass(pass);
+
+    pass.length !== 0 && !valid
+      ? setPassValid('* 대소문자&숫자&특수문자를 포함 7자이상 입력해주세요.')
+      : setPassValid('');
+  }, [pass]);
+
+  useEffect(() => {
+    let same = checkPassCompare(pass, passCheck);
+
+    !same ? setPassSame('* 비밀번호가 일치하지 않습니다.') : setPassSame('');
+  }, [passCheck]);
+
+  useEffect(() => {
+    let valid = checkName(name);
+
+    name.length !== 0 && !valid
+      ? setNameValid('* 올바르지 않은 이름입니다.')
+      : setNameValid('');
+  }, [name]);
+
+  useEffect(() => {
+    let valid1 = checkNickNameLength(nickName);
+    let valid2 = checkNickName(nickName);
+
+    nickName.length !== 0 && !valid1
+      ? setNickNameValid('* 2글자이상 17자미만으로 입력하세요.')
+      : nickName.length !== 0 && !valid2
+      ? setNickNameValid('* 올바르지 않은 닉네임입니다.')
+      : setNickNameValid('');
+  }, [nickName]);
+
+  useEffect(() => {
+    let valid = parseInt(age);
+
+    (age.length !== 0 && valid <= 0) || valid >= 150
+      ? setAgeValid('올바르는지 않는 나이입니다.')
+      : setAgeValid('');
+  }, [age]);
+
+  useEffect(() => {
+    let valid = checkEmail(email);
+
+    email.length !== 0 && !valid
+      ? setEmailValid('* 유효하지 않은 이메일입니다.')
+      : setEmailValid('');
+  }, [email]);
+
+  useEffect(() => {
+    let valid = checkPhoneNumber(phoneNumber);
+
+    phoneNumber.length !== 0 && !valid
+      ? setPhoneNumberValid('* 유효하지 않은 전화번호입니다.')
+      : setPhoneNumberValid('');
+  }, [phoneNumber]);
 
   return (
     <TopContainerView>
@@ -31,6 +141,22 @@ const SignUp = props => {
         backBtn={true}
         backHandler={() => props.navigation.goBack(null)}
       />
+      {/* <WheelPicker
+        width={316}
+        height={344}
+        visible={ageModal}
+        data={}
+        value={}
+        closeHandler={async () => {
+          await this.setState({trackFilter: false});
+
+          let timeout = setInterval(() => {
+            this.setState({filter: true});
+            clearTimeout(timeout);
+          }, 500);
+        }}
+        footerHandler={}
+      /> */}
       <ScrollView
         style={{
           flex: 1,
@@ -58,7 +184,12 @@ const SignUp = props => {
                 marginTop: widthPercentageToDP(10),
                 height: widthPercentageToDP(40),
                 borderWidth: widthPercentageToDP(1),
-                borderColor: id.length === 0 ? '#dbdbdb' : '#53A6EC',
+                borderColor:
+                  id.length === 0
+                    ? '#dbdbdb'
+                    : idValid.length === 0
+                    ? '#53A6EC'
+                    : 'red',
                 borderRadius: widthPercentageToDP(15),
                 paddingLeft: widthPercentageToDP(20),
               }}
@@ -70,15 +201,23 @@ const SignUp = props => {
               }}
               returnKeyType={'next'}
             />
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {idValid}
+            </NBGBText>
             <StandardView
               style={{
                 flexDirection: 'row',
                 justifyContent: 'space-between',
                 alignItems: 'center',
-                marginTop: widthPercentageToDP(30),
+                marginTop: widthPercentageToDP(15),
                 height: widthPercentageToDP(40),
                 borderWidth: widthPercentageToDP(1),
-                borderColor: pass.length === 0 ? '#dbdbdb' : '#53A6EC',
+                borderColor:
+                  pass.length === 0
+                    ? '#dbdbdb'
+                    : passValid.length === 0
+                    ? '#53A6EC'
+                    : 'red',
                 borderRadius: widthPercentageToDP(15),
                 paddingLeft: widthPercentageToDP(20),
                 paddingRight: widthPercentageToDP(5),
@@ -91,7 +230,9 @@ const SignUp = props => {
                 placeholder={'비밀번호'}
                 secureTextEntry={passVisible}
                 value={pass}
-                onChangeText={text => setPass(text)}
+                onChangeText={text => {
+                  setPass(text);
+                }}
                 onSubmitEditing={() => {
                   Keyboard.dismiss();
 
@@ -112,20 +253,30 @@ const SignUp = props => {
                 </BTN>
               )}
             </StandardView>
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {passValid}
+            </NBGBText>
             <TextInput
               style={{
-                marginTop: widthPercentageToDP(30),
-                marginBottom: widthPercentageToDP(15),
+                marginTop: widthPercentageToDP(15),
+                marginBottom: widthPercentageToDP(0),
                 height: widthPercentageToDP(40),
                 borderWidth: widthPercentageToDP(1),
-                borderColor: passCheck.length === 0 ? '#dbdbdb' : '#53A6EC',
+                borderColor:
+                  passCheck.length === 0
+                    ? '#dbdbdb'
+                    : passSame.length === 0
+                    ? '#53A6EC'
+                    : 'red',
                 borderRadius: widthPercentageToDP(15),
                 paddingLeft: widthPercentageToDP(20),
               }}
               placeholder={'비밀번호 재확인'}
               secureTextEntry={true}
               value={passCheck}
-              onChangeText={text => setPassCheck(text)}
+              onChangeText={async text => {
+                await setPassCheck(text);
+              }}
               onSubmitEditing={() => {
                 Keyboard.dismiss();
 
@@ -134,6 +285,9 @@ const SignUp = props => {
               }}
               returnKeyType={'next'}
             />
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {passSame}
+            </NBGBText>
           </StandardView>
           <StandardView
             margin={20}
@@ -148,37 +302,77 @@ const SignUp = props => {
                 marginTop: widthPercentageToDP(15),
                 height: widthPercentageToDP(40),
                 borderWidth: widthPercentageToDP(1),
-                borderColor: id.length === 0 ? '#dbdbdb' : '#53A6EC',
+                borderColor:
+                  name.length === 0
+                    ? '#dbdbdb'
+                    : nameValid.length === 0
+                    ? '#53A6EC'
+                    : 'red',
                 borderRadius: widthPercentageToDP(15),
                 paddingLeft: widthPercentageToDP(20),
               }}
               placeholder={'이름'}
+              value={name}
+              onChangeText={text => setName(text)}
+              onSubmitEditing={() => {
+                // passRef.current.focus();
+              }}
               returnKeyType={'next'}
             />
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {nameValid}
+            </NBGBText>
             <TextInput
               style={{
-                marginTop: widthPercentageToDP(30),
+                marginTop: widthPercentageToDP(15),
                 height: widthPercentageToDP(40),
                 borderWidth: widthPercentageToDP(1),
-                borderColor: id.length === 0 ? '#dbdbdb' : '#53A6EC',
+                borderColor:
+                  nickName.length === 0
+                    ? '#dbdbdb'
+                    : nickNameValid.length === 0
+                    ? '#53A6EC'
+                    : 'red',
                 borderRadius: widthPercentageToDP(15),
                 paddingLeft: widthPercentageToDP(20),
               }}
               placeholder={'닉네임'}
+              value={nickName}
+              onChangeText={text => setNickName(text)}
+              onSubmitEditing={() => {
+                // passRef.current.focus();
+              }}
               returnKeyType={'next'}
             />
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {nickNameValid}
+            </NBGBText>
             <TextInput
               style={{
-                marginTop: widthPercentageToDP(30),
+                marginTop: widthPercentageToDP(15),
+                width: widthPercentageToDP(70),
                 height: widthPercentageToDP(40),
                 borderWidth: widthPercentageToDP(1),
-                borderColor: id.length === 0 ? '#dbdbdb' : '#53A6EC',
+                borderColor:
+                  age.length === 0
+                    ? '#dbdbdb'
+                    : ageValid.length === 0
+                    ? '#53A6EC'
+                    : 'red',
                 borderRadius: widthPercentageToDP(15),
                 paddingLeft: widthPercentageToDP(20),
               }}
               placeholder={'나이'}
-              returnKeyType={'next'}
+              keyboardType={'number-pad'}
+              value={age}
+              onChangeText={text => setAge(text)}
+              onSubmitEditing={() => {
+                // passRef.current.focus();
+              }}
             />
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {ageValid}
+            </NBGBText>
           </StandardView>
           <StandardView
             padding={15}
@@ -189,43 +383,129 @@ const SignUp = props => {
               borderColor: '#53A6EC',
               borderRadius: widthPercentageToDP(5),
             }}>
+            <StandardView
+              style={{
+                flexDirection: 'row',
+                marginTop: widthPercentageToDP(15),
+              }}>
+              <BTN
+                onPress={() => {
+                  setGender(1);
+                }}
+                style={{
+                  flexDirection: 'row',
+                  marginRight: widthPercentageToDP(30),
+                }}>
+                <View
+                  style={{
+                    width: widthPercentageToDP(15),
+                    height: widthPercentageToDP(15),
+                    borderWidth: widthPercentageToDP(1),
+                    borderRadius: widthPercentageToDP(7.5),
+                    marginRight: widthPercentageToDP(10),
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <View
+                    style={{
+                      width: widthPercentageToDP(10),
+                      height: widthPercentageToDP(10),
+                      borderRadius: widthPercentageToDP(5),
+                      backgroundColor: gender !== 1 ? 'white' : '#53A6EC',
+                    }}
+                  />
+                </View>
+                <NBGBText>여자</NBGBText>
+              </BTN>
+              <BTN
+                onPress={() => {
+                  setGender(2);
+                }}
+                style={{
+                  flexDirection: 'row',
+                  marginRight: widthPercentageToDP(30),
+                }}>
+                <View
+                  style={{
+                    width: widthPercentageToDP(15),
+                    height: widthPercentageToDP(15),
+                    borderWidth: widthPercentageToDP(1),
+                    borderRadius: widthPercentageToDP(7.5),
+                    marginRight: widthPercentageToDP(10),
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <View
+                    style={{
+                      width: widthPercentageToDP(10),
+                      height: widthPercentageToDP(10),
+                      borderRadius: widthPercentageToDP(5),
+                      backgroundColor: gender !== 2 ? 'white' : '#53A6EC',
+                    }}
+                  />
+                </View>
+                <NBGBText>남자</NBGBText>
+              </BTN>
+            </StandardView>
+            <TextInput
+              style={{
+                marginTop: widthPercentageToDP(30),
+                height: widthPercentageToDP(40),
+                borderWidth: widthPercentageToDP(1),
+                borderColor:
+                  phoneNumber.length === 0
+                    ? '#dbdbdb'
+                    : phoneNumberValid.length === 0
+                    ? '#53A6EC'
+                    : 'red',
+                borderRadius: widthPercentageToDP(15),
+                paddingLeft: widthPercentageToDP(20),
+              }}
+              placeholder={'전화번호 ( "-" 제외 )'}
+              keyboardType={'number-pad'}
+              value={phoneNumber}
+              onChangeText={text => setPhoneNumber(text)}
+              onSubmitEditing={() => {
+                // passRef.current.focus();
+              }}
+              returnKeyType={'next'}
+            />
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {phoneNumberValid}
+            </NBGBText>
             <TextInput
               style={{
                 marginTop: widthPercentageToDP(15),
                 height: widthPercentageToDP(40),
                 borderWidth: widthPercentageToDP(1),
-                borderColor: id.length === 0 ? '#dbdbdb' : '#53A6EC',
-                borderRadius: widthPercentageToDP(15),
-                paddingLeft: widthPercentageToDP(20),
-              }}
-              placeholder={'성별'}
-              returnKeyType={'next'}
-            />
-            <TextInput
-              style={{
-                marginTop: widthPercentageToDP(30),
-                height: widthPercentageToDP(40),
-                borderWidth: widthPercentageToDP(1),
-                borderColor: id.length === 0 ? '#dbdbdb' : '#53A6EC',
-                borderRadius: widthPercentageToDP(15),
-                paddingLeft: widthPercentageToDP(20),
-              }}
-              placeholder={'전화번호'}
-              returnKeyType={'next'}
-            />
-            <TextInput
-              style={{
-                marginTop: widthPercentageToDP(30),
-                height: widthPercentageToDP(40),
-                borderWidth: widthPercentageToDP(1),
-                borderColor: id.length === 0 ? '#dbdbdb' : '#53A6EC',
+                borderColor:
+                  email.length === 0
+                    ? '#dbdbdb'
+                    : emailValid.length === 0
+                    ? '#53A6EC'
+                    : 'red',
                 borderRadius: widthPercentageToDP(15),
                 paddingLeft: widthPercentageToDP(20),
               }}
               placeholder={'이메일'}
+              keyboardType={'email-address'}
+              value={email}
+              onChangeText={text => setEmail(text)}
+              onSubmitEditing={() => {
+                // passRef.current.focus();
+              }}
               returnKeyType={'done'}
             />
-            <TextInput
+            <NBGBText marginLeft={5} marginTop={5} fontSize={10} color={'red'}>
+              {emailValid}
+            </NBGBText>
+            <View
+              style={{
+                marginTop: widthPercentageToDP(30),
+                height: widthPercentageToDP(25),
+              }}
+            />
+            {/* <TextInput
               style={{
                 marginTop: widthPercentageToDP(30),
                 height: widthPercentageToDP(40),
@@ -236,7 +516,7 @@ const SignUp = props => {
               }}
               placeholder={'아바타'}
               returnKeyType={'next'}
-            />
+            /> */}
           </StandardView>
         </Swiper>
         <StandardView style={{flexDirection: 'row', justifyContent: 'center'}}>
@@ -276,6 +556,8 @@ const SignUp = props => {
             <BTN
               onPress={async () => {
                 // 가입
+
+                props.navigation.goBack(null);
               }}
               style={{
                 width: widthPercentageToDP(50),
