242921019a5c864b7ccc8a0a34f44be71cdfdd88
juntaekHong <wnsxor3489@gmail.com>
2020-04-26 20:02:02 +0900

    1. 마이 페이지 - 내 리뷰 페이지 다트 기능 구현 2. 마이 페이지에서 수정 시, 네비게이션 버그 수정예정

--- src/containers/myPage/MyReview.js
+++ src/containers/myPage/MyReview.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import {
   TopContainerView,
@@ -12,15 +12,93 @@ import {ReviewList} from '../../components/review/FlatList';
 import {CustomModal} from '../../components/common/Modal';
 import {widthPercentageToDP} from '../../utils/util';
 import {NBGText, NBGBText} from '../../components/common/Text';
+import {ReviewActions} from '../../store/actionCreator';
+import {BottomMenuModal} from '../../components/review/Modal';
 
 const MyReview = props => {
+  // 다트 버튼 모달
+  const [dotsModal, setDotsModal] = useState(false);
+  // 삭제 후, 삭제 알림 모달
+  const [reviewDeleteModal, setReviewDeleteModal] = useState(false);
   // 다트 버튼 클릭한 리뷰에 대한 유저 닉네임
   const [reviewUser, setReviewUser] = useState();
+  const user = {user: {userNickName: props.user.userNickName}};
   // 댓글 내용 모두 보기 모달
   const [reviewContentModal, setReviewContentModal] = useState(false);
 
   return (
     <TopContainerView>
+      {/* 다트 모달 */}
+      <BottomMenuModal
+        width={375}
+        padding={10}
+        visible={dotsModal}
+        user={props.user ? props.user : '비회원'}
+        reviewUser={user}
+        // 수정
+        modifyHandler={async () => {
+          await setDotsModal(false);
+          await props.navigation.navigate('ReviewWrite', {
+            modify: true,
+            hpid: reviewUser.hpid,
+            reviewData: reviewUser,
+          });
+        }}
+        // 삭제
+        DeleteHandler={async () => {
+          await setDotsModal(false);
+
+          await ReviewActions.deleteReview(reviewUser.reviewIndex);
+          await ReviewActions.getAllReview(reviewUser.hpid);
+
+          let timeout = setInterval(async () => {
+            await setReviewDeleteModal(true);
+            clearInterval(timeout);
+          }, 500);
+
+          await ReviewActions.getMyReview();
+        }}
+        // 취소
+        closeHandler={async () => {
+          await setDotsModal(false);
+        }}
+      />
+      {/* 리뷰 삭제 모달 */}
+      <CustomModal
+        width={300}
+        height={200}
+        visible={reviewDeleteModal}
+        close={false}
+        children={
+          <StandardView style={{marginLeft: widthPercentageToDP(20)}}>
+            <NBGBText fontSize={17}>리뷰 삭제 왼료</NBGBText>
+            <StandardView style={{marginTop: widthPercentageToDP(30)}}>
+              <NBGText fontSize={13}>
+                정상적으로 해당 리뷰가 삭제되었습니다.
+              </NBGText>
+            </StandardView>
+          </StandardView>
+        }
+        renderFooter={() => {
+          return (
+            <StandardView
+              style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+              <BTN
+                style={{
+                  marginRight: widthPercentageToDP(30),
+                  marginBottom: widthPercentageToDP(20),
+                  justifyContent: 'flex-end',
+                  alignItems: 'flex-end',
+                }}
+                onPress={async () => {
+                  await setReviewDeleteModal(false);
+                }}>
+                <NBGText fontSize={15}>닫기</NBGText>
+              </BTN>
+            </StandardView>
+          );
+        }}
+      />
       {/* 리뷰 content 모달 */}
       <CustomModal
         width={300}
@@ -77,7 +155,11 @@ const MyReview = props => {
           data={props.my_review_list}
           count={0}
           user={props.user}
-          dots={false}
+          dots={true}
+          dotsBtn={(bool, reviewData) => {
+            setDotsModal(bool);
+            setReviewUser(reviewData);
+          }}
           contentBtn={(bool, reviewData) => {
             setReviewContentModal(bool);
             setReviewUser(reviewData);
