8dd4d8df19c08ec8cffa4c65ec4dc5790b560d32
juntaekHong <wnsxor3489@gmail.com>
2020-05-27 21:26:57 +0900

    1. 게시판 페이지 - 기존 초안으로 만들어놨던 게시판 상세페이지 필요없어져서 삭제

--- src/navigations/community/CommunityStack.js
+++ src/navigations/community/CommunityStack.js
@@ -1,11 +1,9 @@
 import {createStackNavigator} from 'react-navigation-stack';
 import Community from '../../containers/community/Community';
-import CommunityDetail from '../../containers/community/CommunityDetail';
 
 const CommunityStack = createStackNavigator(
   {
     Community: {screen: Community},
-    CommunityDetail: {screen: CommunityDetail},
   },
   {
     initialRouteName: 'Community',
