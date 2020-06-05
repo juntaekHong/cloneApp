8a7343a90bf1fa8bfabf37f028daa4ef25d4344b
juntaekHong <wnsxor3489@gmail.com>
2020-06-05 14:22:29 +0900

    주석 코드 삭제

--- src/configs/config.js
+++ src/configs/config.js
@@ -6,16 +6,12 @@ import values from './values';
  */
 
 const init = {
-  type: 'dev',
+  type: 'prod',
   server: {
-    // dev: 'http://ec2-15-164-250-5.ap-northeast-2.compute.amazonaws.com',
-    // prod: 'http://ec2-15-164-250-5.ap-northeast-2.compute.amazonaws.com',
     dev: 'http://ec2-15-164-221-39.ap-northeast-2.compute.amazonaws.com',
     prod: 'https://ppodeum.site',
   },
   mailServer: {
-    // dev: 'http://ec2-15-164-250-5.ap-northeast-2.compute.amazonaws.com',
-    // prod: 'http://ec2-15-164-250-5.ap-northeast-2.compute.amazonaws.com',
     dev: 'http://ec2-15-164-221-39.ap-northeast-2.compute.amazonaws.com',
     prod: 'https://ppodeum.site',
   },
